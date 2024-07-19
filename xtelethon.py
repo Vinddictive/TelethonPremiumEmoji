8dd46cc9601ab46fc6638f7eb1a53136c7e14471
Ken <54377513+Vinddictive@users.noreply.github.com>
2024-07-19 21:01:41 +0700

    Create xtelethon.py

---
+++ xtelethon.py
@@ -0,0 +1,50 @@
+from telethon.extensions import markdown, html
+from telethon import types
+
+
+class InvalidFormatException(Exception):
+    pass
+
+
+class CustomParseMode:
+    """
+    Example using Markdown:
+
+    - client.send_message('me', 'hello this is a [Text](spoiler), with custom emoji [❤️](emoji/10002345) !')
+
+    Example using HTML:
+
+    - client.send_message('me', 'hello this is a <a href="spoiler">Text</a>, with custom emoji <a href="emoji/10002345">❤️</a> !')
+
+    `Sending spoilers and custom emoji <https://github.com/LonamiWebs/Telethon/wiki/Sending-more-than-just-messages#sending-spoilers-and-custom-emoji>`_
+    :param parse_mode: The format to use for parsing text.
+                       Can be either 'markdown' for Markdown formatting
+                       or 'html' for HTML formatting.
+    """
+    def __init__(self, parse_mode: str):
+        self.parse_mode = parse_mode
+
+    def parse(self, text):
+        if self.parse_mode == 'markdown':
+            text, entities = markdown.parse(text)
+        elif self.parse_mode == 'html':
+            text, entities = html.parse(text)
+        else:
+            raise InvalidFormatException("Invalid parse mode. Choose either Markdown or HTML.")
+
+        for i, e in enumerate(entities):
+            if isinstance(e, types.MessageEntityTextUrl):
+                if e.url == 'spoiler':
+                    entities[i] = types.MessageEntitySpoiler(e.offset, e.length)
+                elif e.url.startswith('emoji/'):
+                    entities[i] = types.MessageEntityCustomEmoji(e.offset, e.length, int(e.url.split('/')[1]))
+        return text, entities
+
+    @staticmethod
+    def unparse(text, entities):
+        for i, e in enumerate(entities or []):
+            if isinstance(e, types.MessageEntityCustomEmoji):
+                entities[i] = types.MessageEntityTextUrl(e.offset, e.length, f'emoji/{e.document_id}')
+            if isinstance(e, types.MessageEntitySpoiler):
+                entities[i] = types.MessageEntityTextUrl(e.offset, e.length, 'spoiler')
+        return html.unparse(text, entities)
